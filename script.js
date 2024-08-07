f4430959334bd400e05ec772f5da9662f8f2b7f2
Edgar <150823744+ebar365@users.noreply.github.com>
2024-08-07 12:15:51 -0400

    Create basic functions that work on the console. Create add, substract, multiply and divide functions on js file.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log('hola mundo');
-
+// start by creating functions
+// and testing them in your browser’s console.
+function add(a,b){
+    return a+b;
+}
+function substract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
